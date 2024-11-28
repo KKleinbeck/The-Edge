79b9823669a9cad56fdd6eecaeb76e94949995a5
Kevin Kleinbeck <kekleinbeck@gmail.com>
2024-11-28 16:58:25 +0100

    Small improvements to grenade chat box

--- modules/hooks/chat-hooks.js
+++ modules/hooks/chat-hooks.js
@@ -25,24 +25,9 @@ export default function() {
             elem.append(`<div>${proficiencyRoll.quality} QL</div>`)
             break;
           case "Failure":
-            elem.append(`<div>${proficiencyRoll.quality} FL</div>`)
+            elem.append(`<div>${-proficiencyRoll.quality} FL</div>`)
         }
       }
     })
-      // html.find(".damage-apply-box").click(ev => {
-      //     // because for some stupid reasons we cannot use data tags here, in chat messages
-      //     const parts = ev.currentTarget.className.split(" ")
-      //     const targetIDs = parts[2].split("targetID-")
-      //     const dmgRes = parts[3].split("dmg-")
-      //     const sceneID = parts[4].split("sceneID-")[1]
-      //     targetIDs.shift()
-      //     dmgRes.shift()
-      //     console.log(ev.currentTarget)
-      //     let scene = getScene(sceneID)
-      //     let targets = getTargets(scene, targetIDs)
-      //     for (const target of targets) {
-      //         for (const dmg of dmgRes) applyDamage(target, dmg);
-      //     }
-      // })
   })
 }
