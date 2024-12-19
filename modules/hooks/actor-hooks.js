9755f9b50908bcb074c0a6ea3b87214fe6e5318b
Kevin Kleinbeck <kekleinbeck@gmail.com>
2024-12-19 05:32:21 +0100

    Better hooking for status updates

--- modules/hooks/actor-hooks.js
+++ modules/hooks/actor-hooks.js
@@ -1,10 +1,14 @@
 import Aux from "../system/auxilliaries.js";
 
 export default function() {
-  Hooks.on("refreshToken", async (token, refreshInfo) => {
+  Hooks.on("renderActorSheet", async (sheet, html, actorData) => {
+    console.log("Hello")
     if (Aux.hasRaceCondDanger("refreshToken")) return undefined;
 
-    let actor = token.actor;
+    let actor = actorData.actor;
+    if (actorData.token) {
+      actor = Aux.getActor(actorData.token.actorId, actorData.token.id)
+    }
     await actor._determineEncumbrance();
     await actor._updateStatus();
     await actor._updateStrain();
