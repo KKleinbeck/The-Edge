0707b9d07c07da28811f4379fc2da676af70022f
Kevin Kleinbeck <KeKleinbeck@gmail.com>
2026-09-14 06:39:17 +0200

    Fixed bug with changing max level of skills

--- src/items/item-sheets/others.js
+++ src/items/item-sheets/others.js
@@ -285,17 +285,22 @@ export class ItemSheetSkill extends TheEdgeItemSheet {
     );
   }
 
+  _attachAdditionalFrameListeners() {
+    super._attachAdditionalFrameListeners();
+
+    this.element
+      .querySelectorAll("#maxLevel")
+      ?.forEach((x) =>
+        x.addEventListener("change", (ev) => this._onMaxLevelChange(ev)),
+      );
+  }
+
   _onRender(context, options) {
     super._onRender(context, options);
     // this.element.find(".effect-hint").click(ev => {
     //   this.options.displayHint = !this.options.displayHint;
     //   this._render()
     // });
-    this.element
-      .querySelectorAll(".max-level")
-      ?.forEach((x) =>
-        x.addEventListener("change", (ev) => this._onMaxLevelChange(ev)),
-      );
     this.element
       .querySelectorAll(".effect-level-modify")
       ?.forEach((x) =>
